c44a12c70d72aa0c37eac0b3df2cd50a75e8fc9f
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2012-06-09 16:05:55 +0200

    make redirect_url depends on GET parematr in fb_login url

--- facebook/views.py
+++ facebook/views.py
@@ -5,14 +5,21 @@ from django.conf import settings
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import authenticate
 from django.core.urlresolvers import reverse
+from django.contrib import messages
+from django.utils.translation import ugettext as _
 
 def login(request):
     """ First step of process, redirects user to facebook, which redirects to authentication_callback. """
 
+    redirect_uri = request.GET.get('redirect_uri','/facebook/authentication_callback')
+
+    if not redirect_uri:
+        redirect_uri = '/facebook/authentication_callback'
+
     args = {
         'client_id': settings.FACEBOOK_APP_ID,
         'scope': settings.FACEBOOK_SCOPE,
-        'redirect_uri': request.build_absolute_uri('/facebook/authentication_callback'),
+        'redirect_uri': request.build_absolute_uri(redirect_uri),
     }
     return HttpResponseRedirect('https://www.facebook.com/dialog/oauth?' + urllib.urlencode(args))
 
@@ -34,8 +41,10 @@ def authentication_callback(request):
         if user.is_active:
             auth_login(request, user)
             #figure out where to go after setup
+            messages.success(request, _('Your registration has been successful') )
             url = getattr(settings, "LOGIN_REDIRECT_URL", "/")
         else:
+            messages.error(request, _('Your account has been locked. Please contact your system administrator') )
             url = "/"
 
         resp = HttpResponseRedirect(url)
