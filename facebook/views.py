fb981efe6df7ee62c502ba25827124828f195618
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2013-03-21 22:51:36 +0100

    Update views.py

--- facebook/views.py
+++ facebook/views.py
@@ -22,35 +22,3 @@ def login(request):
         'redirect_uri': request.build_absolute_uri(redirect_uri),
     }
     return HttpResponseRedirect('https://www.facebook.com/dialog/oauth?' + urllib.urlencode(args))
-
-def authentication_callback(request):
-    """ Second step of the login process.
-    It reads in a code from Facebook, then redirects back to the home page. """
-
-    if request.GET.get("error",None):
-        messages.error(request, _('Your registration has been cancelled.'))
-        url = getattr(settings, "LOGIN_REDIRECT_URL", "/")
-        resp = HttpResponseRedirect(url)
-        return resp
-
-    code = request.GET.get('code')
-    user = authenticate(token=code, request=request)
-
-    if user.is_anonymous():
-        #we have to set this user up
-        url = reverse('facebook_setup')
-        url += "?code=%s" % code
-        resp = HttpResponseRedirect(url)
-
-    else:
-        if user.is_active:
-            auth_login(request, user)
-            #figure out where to go after setup
-            messages.success(request, _('Your registration has been successful') )
-            url = getattr(settings, "LOGIN_REDIRECT_URL", "/")
-        else:
-            messages.error(request, _('Your account has been locked. Please contact your system administrator') )
-            url = "/"
-        resp = HttpResponseRedirect(url)
-
-    return resp
